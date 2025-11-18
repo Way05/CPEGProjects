6ab987a71023f40a2b7f6b2275c8b0e371a5f955
Wei-en Tan <wtan235@gmail.com>
2025-11-18 17:40:25 -0500

    code cleanup

--- Project5/src/main.cpp
+++ Project5/src/main.cpp
@@ -1,50 +1,40 @@
 #include <SSD_Array.h>
 #include <Arduino.h>
 #include <Wire.h>
+
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include "SparkFun_BMI270_Arduino_Library.h"
-
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1:
-// Cannot use 6 as output for ESP. Pins 6-11 are connected to SPI flash. Use 16 instead.
-#define LED_PIN PA0
 
+#define LED_PIN PA0
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define BME280_ADDRESS (0x76) // Primary I2C Address
-
 #define LED_COUNT 4
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_BME280 bme; // I2C
 
 int patternCurrent = 0; // Current Pattern Number
 int SSD_Display = 0;
 int SSD_Decimal = 0;
 uint16_t pixelNumber = LED_COUNT; // Total Number of Pixels
-// Forward declarations for functions used before their definitions
-
 int digitSelect = 0;
+int lastDebounce;
+int debounceDelay = 50;
+unsigned long delayTime = 5000;
 
 void printValues();
 void selectPattern();
 bool begin(uint8_t addr = BME280_ADDRESS, TwoWire *theWire = &Wire);
-Adafruit_BME280 bme; // I2C
-unsigned long delayTime;
-
 HardwareTimer timer(TIM1);
 
 void OnTimer1Interrupt()
 {
   SSD_update(digitSelect, SSD_Display, SSD_Decimal);
-  digitSelect = (digitSelect + 1) % 4; // Cycle through digitSelect values 0 to 3
+  digitSelect = (digitSelect + 1) % 4;
 }
 
-int lastDebounce;
-int debounceDelay = 50;
 void buttonISR()
 {
   long curr = millis();
@@ -53,9 +43,8 @@ void buttonISR()
     int buttonState = digitalRead(PC13);
     if (buttonState == LOW)
     {
-
-      selectPattern();
       patternCurrent = (patternCurrent + 1) % 4;
+      selectPattern();
       lastDebounce = millis();
     }
   }
@@ -66,7 +55,6 @@ void setup()
   Serial.begin(9600);
   while (!Serial)
     ; // time to get serial running
-  Serial.println(F("BME280 test"));
   unsigned status;
   // default settings
   status = bme.begin(BME280_ADDRESS);
@@ -84,12 +72,8 @@ void setup()
     while (1)
       delay(10);
   }
-  Serial.println("-- Default Test --");
-  delayTime = 1000;
-  Serial.println();
   Serial.println("CPEG222 Project 5 - BME280 Values");
 
-  // END of Trinket-specific code.
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all  ASAP
   strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
@@ -100,8 +84,8 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(PC13), buttonISR, CHANGE);
 
   // Configure timer
-  timer.setPrescaleFactor(15); // Set prescaler to 2564 => timer frequency = 168MHz/2564 = 65522 Hz (from prediv'd by 1 clocksource of 168 MHz)
-  timer.setOverflow(500);      // Set overflow to 32761 => timer frequency = 65522 Hz / 32761 = 2 Hz
+  timer.setPrescaleFactor(15); // Set prescaler to 15
+  timer.setOverflow(500);      // Set overflow to 500
   timer.attachInterrupt(OnTimer1Interrupt);
   timer.refresh(); // Make register changes take effect
   timer.resume();  // Start
@@ -143,6 +127,7 @@ void selectPattern()
 void loop()
 {
   printValues();
+  selectPattern();
   delay(delayTime);
 }
 
